b6335e7485db62fecfed0588b19cee5998f9293e
Nabeel <nkhan054002@gmail.com>
2025-05-14 02:43:52 +0530

    model for coupon done

--- internal/models/coupon.go
+++ internal/models/coupon.go
@@ -16,19 +16,21 @@ const (
 
 // Coupon struct now reflects optional fields based on UsageType
 type Coupon struct {
-	Code              string      `json:"code"`
-	UsageType         UsageType  `json:"usage_type"`
-	MaxUsagePerUser   *int       `json:"max_usage_per_user,omitempty"` // Always optional for all types
-	MinOrderValue     *float64   `json:"min_order_value,omitempty"`     // Optional, based on usage type
-	TermsAndConditions string     `json:"terms_and_conditions"`
-	ExpiryDate        *time.Time `json:"expiry_date,omitempty"`         // Optional, based on usage type
-	Discounts         []Discount `json:"discounts"`   // Represents the related discounts
-	TimeWindows       []TimeWindow `json:"time_windows,omitempty"` // Represents the related time windows for time-based coupons
+	Code                    string        `json:"code"`
+	UsageType               UsageType    `json:"usage_type"`
+	MaxUsagePerUser         *int         `json:"max_usage_per_user,omitempty"` // Always optional for all types
+	MinOrderValue           *float64     `json:"min_order_value,omitempty"`     // Optional, based on usage type
+	TermsAndConditions      string       `json:"terms_and_conditions"`
+	ExpiryDate              *time.Time   `json:"expiry_date,omitempty"`         // Optional, based on usage type
+	Discounts               []Discount   `json:"discounts"`                     // Represents the related discounts
+	TimeWindows             []TimeWindow `json:"time_windows,omitempty"`        // Represents the related time windows for time-based coupons
+	ApplicableForCategories []int        `json:"applicable_for_categories,omitempty"` // List of category IDs where the coupon applies
+	ApplicableForMedicineIds []string    `json:"applicable_for_medicine_ids,omitempty"` // List of medicine IDs where the coupon applies
 }
 
 // Discount struct for discount details
 type Discount struct {
-	DiscountType  string  `json:"discount_type"` // 'items' or 'charges'
+	DiscountType  string  `json:"discount_type"` // 'items' or 'charges' // can make this also into a separate type but tbh its fine for now
 	DiscountValue float64 `json:"discount_value"`
 }
 
@@ -60,6 +62,12 @@ func (c *Coupon) Validate() error {
 	if c.MinOrderValue == nil {
 		return fmt.Errorf("MinOrderValue is required")
 	}
+
+	// in the starting we can have it not applicable for any medicine_id or category
+	// // Check applicable categories and medicine IDs if necessary
+	// if len(c.ApplicableForCategories) == 0 && len(c.ApplicableForMedicineIds) == 0 {
+	// 	return fmt.Errorf("At least one of 'applicable_for_categories' or 'applicable_for_medicine_ids' must be provided")
+	// }
 
 	return nil
 }
